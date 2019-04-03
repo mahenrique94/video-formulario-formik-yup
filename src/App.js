de3bf809f7a4e158ea2ae9451d3a8e9bf2825410
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2019-04-02 22:36:37 -0300

    Enviando para o Github

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Form from './components/form'
 
-export default App;
+import "./App.css"
+
+const handleSubmit = values => alert(JSON.stringify(values))
+
+const App = () => (
+    <div className="App">
+        <Form handleSubmit={handleSubmit} initialValues={{}}/>
+    </div>
+)
+
+export default App
---
+++ src/components/form/Form.js
@@ -0,0 +1,40 @@
+import * as yup from 'yup'
+
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import { ErrorMessage, Formik, Form as FormikForm, Field } from 'formik'
+
+import './Form.css'
+
+const validations = yup.object().shape({
+    user: yup.string().email().required(),
+    password: yup.string().min(8).required()
+})
+
+const Form = ({ handleSubmit, initialValues }) => (
+    <Formik initialValues={initialValues} onSubmit={values => handleSubmit(values)} validationSchema={validations}> 
+        {() => (
+            <FormikForm className="Form">
+                <h1 className="Form-Welcome">Welcome</h1>
+                <h2 className="Form-Info">Type your user and password to access the system</h2>
+                <div className="Form-Group">
+                    <Field className="Form-Field" name="user" placeholder="User" type="text"/>
+                    <ErrorMessage className="Form-Error" component="span" name="user"/>
+                </div>
+                <div className="Form-Group">
+                    <Field className="Form-Field" name="password" placeholder="Password" type="password"/>
+                    <ErrorMessage className="Form-Error" component="span" name="password"/>
+                </div>
+                <button className="Form-Btn" type="submit">Login</button>
+            </FormikForm>
+        )}   
+    </Formik>
+)
+
+Form.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    initialValues: PropTypes.object.isRequired
+}
+
+export default Form
